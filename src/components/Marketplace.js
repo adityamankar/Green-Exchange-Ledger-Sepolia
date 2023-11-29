6bf2594f1c5e53f904661fbc3f94498cd03d4a10
adityamankar <aditya.mankar94@gmail.com>
2023-11-28 21:58:39 -0800

    cosmetic change

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -9,11 +9,11 @@ import { useEffect, useState } from 'react';
 export default function Marketplace() {
 const sampleData = [
     {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
+        "name": "CC#1",
+        "description": "Carbon Credit",
+        "website":"",
+        "image":"https://gateway.pinata.cloud/ipfs/QmZNS8281J4LkHbevPrWG14CQg17VSfa8fWXHGVJhFamd3",
+        "price":"0.015ETH",
         "currentlySelling":"True",
         "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
     },
